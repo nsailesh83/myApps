7821ba0544d0fb8775b714284b3438e943551593
Durga Sailesh <ndurgasailesh@hotmail.com>
2018-06-11 15:32:31 +1000

    Adding Component in UI

--- myAppUI/src/app/app.module.ts
+++ myAppUI/src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { Http, HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { ValueComponent } from './value/value.component';
+
 
 @NgModule({
   declarations: [
     AppComponent
-  ],
+,
+    ValueComponent
+],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpModule
   ],
   providers: [],
   bootstrap: [AppComponent]
